abe7274a43a0dc720faba62e68c77d1b840f18db
kdkireye <kdkireye@mts.ru>
2019-09-27 14:46:58 +0300

    Задание №2: Создать первый работающий тест.

--- addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Linq;
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace addressbook_web_tests
 {
+	[TestClass]
 	public class UnitTest1
 	{
-		[Fact]
+		[TestMethod]
 		public void TestMethod1()
 		{
 		}
